dde3ec043d31e4b476ad65b3a26d8aeda4390967
Ivan Ferre <ivan.ferre@powercoders.org>
2023-05-05 14:11:59 +0200

    Get initial values.

--- tipcalculator/script.js
+++ tipcalculator/script.js
@@ -1,12 +1,30 @@
 // Tip Calculator
 
-// input taxRate // if found in local storage, use it as default value.
+let defaultTaxRate = 0.18;
+let defaultTipRate = 0.10;
 
+// Check if there are default values in local storage
+// if (localStorage.getItem("taxRate")) {
+//     taxRate = JSON.parse(localStorage.getItem("defaults"));
+// } else {
+//     taxRate = defaultTaxRate;
+// }
 
-// input usualTipRate // if found in local storage, use it as default value.
+// get default taxRate
+tr = localStorage.getItem("taxRate");
+taxRateValue = ( tr ? JSON.parse(tr) : defaultTaxRate );
+taxRateField = document.getElementById("taxrate");
+taxRateField.setAttribute("value", taxRateValue);
+
+// get default taxRate
+tr = localStorage.getItem("tipRate");
+tipRateValue = (tr ? JSON.parse(tr) : defaultTipRate);
+tipRateField = document.getElementById("tiprate");
+tipRateField.setAttribute("value", tipRateValue);
 
 // input billAmount
-billInput = document.querySelector("bill");
+billField = document.getElementById("bill");
+billField.setAttribute("value", "0.00");
 
 // input t "Include taxRate to compute tipAmount?"
 
