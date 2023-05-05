651e8b31581433dc070cedc652ed4d2fcb0d6759
Ivan Ferre <ivan.ferre@powercoders.org>
2023-05-05 12:25:48 +0200

    Initial draft, still no code.

---
+++ tipcalculator/script.js
@@ -0,0 +1,20 @@
+// Tip Calculator
+
+// input taxRate // if found in local storage, use it as default value.
+
+
+// input usualTipRate // if found in local storage, use it as default value.
+
+// input billAmount
+billInput = document.querySelector("bill");
+
+// input t "Include taxRate to compute tipAmount?"
+
+// if t
+//     tip <- billAmount * (1+taxRate) * (1+usualTipRate)
+// else
+//     tip <- billAmount * (1+usualTipRate)
+
+// round tip to Rappen
+
+// Display rounded tip
